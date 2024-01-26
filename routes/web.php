ee29ef9c7a0edc02314219fe68ed425022c49a08
Fares Mohamed <88165859+fares-soltan@users.noreply.github.com>
2024-01-26 08:31:08 +0200

    Chat box style

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Chat\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/chat',Index::class)->name('chat.index');
