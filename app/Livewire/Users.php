87ed048c8c0946bedc0c9fe65d3900f91e87cc8b
Fares Mohamed <88165859+fares-soltan@users.noreply.github.com>
2024-01-26 11:19:19 +0200

    Function Message in Users Livewire
    - Check if Conversation Exist and redirect to chat if not Create one and redirect to chat

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -2,14 +2,46 @@
 
 namespace App\Livewire;
 
+use App\Models\Conversation;
 use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
+
+    public function message($userId)
+    {
+        $authUserId = auth()->user()->id;
+
+        # Check conversation existing
+        $existingConversation =Conversation::where(function ($query) use ($authUserId,$userId){
+            $query->where('sender_id',$userId)
+                ->where('receiver_id',$authUserId);
+        })->orWhere(function ($query) use ($authUserId,$userId){
+            $query->where('sender_id',$authUserId)
+                ->where('receiver_id',$userId);
+        })->first();
+
+
+        if ($existingConversation){
+            return redirect()->route('chat',['query'=>$existingConversation->id]);
+        }
+
+        # Create Conversation
+
+        $createdConversation = Conversation::create([
+            'sender_id'=>$authUserId,
+            'receiver_id'=>$userId
+        ]);
+
+        return redirect()->route('chat',['query'=>$createdConversation->id]);
+
+
+    }
+
     public function render()
     {
-        $users = User::all();
+        $users = User::where('id','!=',auth()->user()->id)->get();
 
         return view('livewire.users',compact('users'));
     }
--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,9 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+Route::middleware('auth')->group(function () {
+
 Route::get('/chat',Index::class)->name('chat.index');
 Route::get('/chat/{query}',Chat::class)->name('chat');
 Route::get('/users',Users::class)->name('users');
+});
